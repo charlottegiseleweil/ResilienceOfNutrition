5c781be4a582717a39b9dcaae425af1a6629bc26
Charlotte Gisele Weil <weilcharlotte@gmail.com>
2019-01-14 15:20:21 -0800

    Applying xgb3 on future_dfs (All Changing) + climate ensembling

--- modeling_utils.py
+++ modeling_utils.py
@@ -152,25 +152,7 @@ def make_results_df(modelnm,X,y=None,y_2000=None):
     
     return compare
 
-def make_results_df_future(modelnm,X,y=None,y_2000=None):
-    
-    ## Check if exists already, just load the csv:
-    if (modelnm+'.csv') in os.listdir('../Data/outputs/Model_results/2000'):
-        compare = pd.read_csv('../Data/outputs/Model_results/2000/'+modelnm+'.csv')
-        compare = compare.set_index('pixel_id')
 
-    ## If not, make it and save it:
-    else:
-        print('Applying '+modelnm)
-        regression = load_regression(modelnm)
-        y_predicted = regression.predict(X)
-
-        print('Saving model results for '+modelnm)
-        compare = predictions_df(y_predicted,y,y_2000)
-        
-        compare.to_csv('../Data/outputs/Model_results/2000/'+modelnm+'.csv')
-    
-    return compare
 
 
 def predictions_df(y_predicted,y=None,y_2000=None):
@@ -198,4 +180,71 @@ def predictions_df(y_predicted,y=None,y_2000=None):
         compare['Δlog(cal_per_ha)'] = compare['log(cal_per_ha) predicted'] - compare['log(cal_per_ha) in 2000']
         compare['Δcal_per_ha'] = compare['cal_per_ha predicted'] - compare['cal_per_ha in 2000']
 
+    return compare
+
+
+def make_results_df_future(modelnm,X,
+                           Changing=None,
+                           ssp=None,
+                           gcm=None):
+    
+    ## Check if exists already, just load the csv:
+    if (ssp+'_'+gcm+'.csv') in os.listdir('../Data/outputs/Model_results/'+Changing):
+        compare = pd.read_csv('../Data/outputs/Model_results/'+Changing+'/'+ssp+'_'+gcm+'.csv')
+        compare = compare.set_index('pixel_id')
+
+    ## If not, make it and save it:
+    else:
+        print('Applying '+modelnm +' on '+ssp+' '+gcm)
+        regression = load_regression(modelnm)
+        y_predicted = regression.predict(X)
+
+        ## to do: check that baseline df exists otherwise load it
+        #baseline_df = pd.read_csv('../Data/intermediate/baseline_df.csv')
+        #baseline_df = baseline_df.set_index('pixel_id')
+        print('Saving predictions for '+ssp+' '+gcm)
+        compare = predictions_df_future(y_predicted,future_df['%cropland'])
+        
+        compare.to_csv('../Data/outputs/Model_results/'+Changing+'/'+modelnm+'/'+ssp+'_'+gcm+'.csv')
+    
+    return compare
+
+def predictions_df_future(y_predicted,cropland_serie,change_cols=True):
+    
+    compare = pd.DataFrame()
+    
+    compare['%cropland'] = cropland_serie #so we have pixel_id on index
+    
+    compare['log(cal_per_ha) predicted'] = y_predicted
+    compare['cal_per_ha predicted'] = compare['log(cal_per_ha) predicted'].apply(lambda x: np.exp(x))
+    
+    # Check that baseline df exists (with index 'pixel_id') otherwise load it
+    try:
+        baseline_df
+    except NameError:
+        baseline_df = pd.read_csv('../Data/intermediate/baseline_df.csv')
+        baseline_df = baseline_df.set_index('pixel_id')
+    if baseline_df.index.name != 'pixel_id':
+        baseline_df = baseline_df.set_index('pixel_id')
+        
+        
+    compare = compare.merge(pd.DataFrame(baseline_df['log_calories_per_ha']), how='outer',
+                        right_index=True,left_index=True)
+    compare  = compare.rename({'log_calories_per_ha':'log(cal_per_ha) in 2000'},axis=1)
+    # Or replace 2 lines above by this one (en fait ça marchait aussi):
+    #compare['log(cal_per_ha) in 2000'] = baseline_df['log_calories_per_ha']
+    compare['cal_per_ha in 2000'] = compare['log(cal_per_ha) in 2000'].apply(lambda x: np.exp(x))
+    
+    #Replace NaN by 0
+    compare['log(cal_per_ha) in 2000'] = compare['log(cal_per_ha) in 2000'].replace({np.nan: 0}) 
+    compare['cal_per_ha in 2000'] = compare['cal_per_ha in 2000'].replace({np.nan: 0}) 
+    
+    if change_cols == True:
+        compare['Δlog(cal_per_ha)'] = compare['log(cal_per_ha) predicted'] - compare['log(cal_per_ha) in 2000']
+        compare['Δcal_per_ha'] = compare['cal_per_ha predicted'] - compare['cal_per_ha in 2000']
+
+        compare['%Δcal_per_ha'] = (compare['cal_per_ha predicted'] - compare['cal_per_ha in 2000']) / compare['cal_per_ha in 2000']
+        # Replace infinty values with 9999
+        compare['%Δcal_per_ha'] = compare['%Δcal_per_ha'].apply(lambda x : 9999 if x>9999 else x)
+    
     return compare
